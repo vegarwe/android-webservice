a091eee3d4a9af010727447f320e8a30b76e568b
Vegar Westerlund <vegarwe@gmail.com>
2014-10-26 13:39:55 +0100

    Cleaned up even more

--- app/src/main/java/no/raiom/webservice/GattServer.java
+++ app/src/main/java/no/raiom/webservice/GattServer.java
@@ -8,13 +8,10 @@ import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothGattServerCallback;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.util.Log;
 
 import java.util.UUID;
-
-import no.raiom.utils.ByteUtils;
 
 public class GattServer {
     public final static UUID WEBSERVICE_SERV   = UUID.fromString("785f7d50-6736-4ebc-ad98-1bdc9306a622");
@@ -69,9 +66,8 @@ public class GattServer {
 
         @Override
         public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
-            characteristic.setValue(value);
+            gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, value);
             if (characteristic.getUuid().equals(WEBSERVICE_BODY)) {
-                gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, value);
                 HttpPutAsyncTask httpPut = new HttpPutAsyncTask(GattServer.this, new String(value));
                 httpPut.execute();
             }
@@ -79,9 +75,6 @@ public class GattServer {
 
         @Override
         public void onDescriptorWriteRequest(BluetoothDevice device, int requestId, BluetoothGattDescriptor descriptor, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
-            Log.i("Fjase", "CCCD set");
-            super.onDescriptorWriteRequest(device, requestId, descriptor, preparedWrite, responseNeeded, offset, value);
-            descriptor.setValue(value);
             gattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, value);
         }
 
